f97bc3e5e64ec82aa5edc2ff9061af8665b853c0
HB-1001 <75037684+HB-1001@users.noreply.github.com>
2021-05-21 21:52:17 +0400

    test

--- backend/build/index.js
+++ backend/build/index.js
@@ -25,6 +25,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // import { json } from 'body-parser';
 const express_1 = __importStar(require("express"));
 const http_1 = __importDefault(require("http"));
+const path_1 = __importDefault(require("path"));
 const payment_1 = require("./routes/payment");
 const app = express_1.default();
 app.use((req, res, next) => {
@@ -35,10 +36,11 @@ app.use((req, res, next) => {
         express_1.json()(req, res, next);
     }
 });
-app.get('/', (_req, res) => {
-    res.send('hello');
+app.use(express_1.default.static(path_1.default.join(__dirname, '..', '..', 'frontend', 'build')));
+app.get('*', function (_request, response) {
+    response.sendFile(path_1.default.join(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
 });
 app.use('/payment', payment_1.router);
-http_1.default.createServer(app).listen(8080, () => {
+http_1.default.createServer(app).listen(process.env.PORT || 8080, () => {
     console.log('running app on 8080');
 });
